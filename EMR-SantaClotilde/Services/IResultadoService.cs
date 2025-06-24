27dfec46f11180df59f8a4be4da62e1a59e5339e
StifCerna <stifcernab@gmail.com>
2025-06-23 22:23:00 -0500

    Update IResultadoService.cs

--- EMR-SantaClotilde/Services/IResultadoService.cs
+++ EMR-SantaClotilde/Services/IResultadoService.cs
@@ -1,15 +1,16 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using EMR_SantaClotilde.Models;
 
 namespace EMR_SantaClotilde.Services.Interfaces
 {
     public interface IResultadoService
     {
-        void Agregar(Resultado resultado);
-        void Modificar(Resultado resultado);
-        void Eliminar(int id);
-        Resultado? ObtenerPorId(int id);
-        IEnumerable<Resultado> ObtenerTodos();
-        IEnumerable<Resultado> BuscarPorPaciente(int pacienteId);
+        Task<ResultadoOperacion> AgregarAsync(Resultado resultado);
+        Task<ResultadoOperacion> ModificarAsync(Resultado resultado);
+        Task<bool> EliminarAsync(int id);
+        Task<Resultado?> ObtenerPorIdAsync(int id);
+        Task<IEnumerable<Resultado>> ObtenerTodosAsync();
+        Task<IEnumerable<Resultado>> BuscarPorPacienteAsync(int pacienteId);
     }
 }
