c2a45c1a4454d823a3fddcfc0aeb88e26867a5f9
StifCerna <stifcernab@gmail.com>
2025-06-23 22:12:42 -0500

    Update Resultados.cs

--- EMR-SantaClotilde/Forms/Resultados.cs
+++ EMR-SantaClotilde/Forms/Resultados.cs
@@ -7,23 +7,63 @@ namespace EMR_SantaClotilde
     public partial class Resultados : Form
     {
         private readonly ICitaService _citaService;
-        public Resultados(ICitaService citaService)
+        private readonly IResultadoService _resultadoService;
+
+        public Resultados(ICitaService citaService, IResultadoService resultadoService)
         {
             _citaService = citaService;
+            _resultadoService = resultadoService;
+        
             InitializeComponent();
+        
+            this.Load += Resultados_Load;
         }
+
+        private void Resultados_Load(object sender, EventArgs e)
+        {
+            CargarResultados();
+        }
+
         private void btnAgregar_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Agregar resultado...");
         }
 
+                private void CargarResultados()
+        {
+            var resultados = _resultadoService.ObtenerTodos();
+            dgvResultados.DataSource = resultados.ToList();
+        }
+
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Buscando resultado...");
+            if (cmbPaciente.SelectedItem is Paciente paciente)
+            {
+                var resultados = _resultadoService.BuscarPorPaciente(paciente.Id);
+                dgvResultados.DataSource = resultados.ToList();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un paciente.");
+            }
         }
-        private void btnModificar_Click(object sender, EventArgs e)
+        
+        private void btnEliminar_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Modificando resultado...");
+            if (dgvResultados.CurrentRow?.DataBoundItem is Resultado resultado)
+            {
+                var confirm = MessageBox.Show("¿Desea eliminar este resultado?", "Confirmar", MessageBoxButtons.YesNo);
+                if (confirm == DialogResult.Yes)
+                {
+                    _resultadoService.Eliminar(resultado.Id);
+                    MessageBox.Show("Resultado eliminado correctamente.");
+                    CargarResultados();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un resultado para eliminar.");
+            }
         }
 
         private void btnResultados_Click(object sender, EventArgs e)
