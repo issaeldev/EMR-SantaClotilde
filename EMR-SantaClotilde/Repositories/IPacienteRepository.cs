e8e7a860cf86282f63cf44ca79eb0315965af32c
Melany Lucio <104110901+MelanyLucio@users.noreply.github.com>
2025-06-25 00:16:44 -0500

    Add files via upload

---
+++ EMR-SantaClotilde/Repositories/IPacienteRepository.cs
@@ -0,0 +1,20 @@
+﻿using EMR_SantaClotilde.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMR_SantaClotilde.Repositories
+{
+    public interface IPacienteRepository
+    {
+        Paciente GetById(int id);
+        List<Paciente> GetAll();
+        Paciente GetByDni(string dni);
+        List<Paciente> SearchByNombre(string nombre);
+        void Add(Paciente paciente);
+        void Update(Paciente paciente);
+        void Delete(int id);
+    }
+}
---
+++ EMR-SantaClotilde/Repositories/PacienteRepository.cs
@@ -0,0 +1,79 @@
+﻿using EMR_SantaClotilde.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMR_SantaClotilde.Repositories
+{
+    internal class PacienteRepository : IPacienteRepository
+    {
+        private readonly EmrSantaClotildeContext _context;
+
+        public PacienteRepository(EmrSantaClotildeContext context)
+        {
+            _context = context;
+        }
+
+        public Paciente GetById(int id)
+        {
+            return _context.Set<Paciente>()
+                .Include(p => p.Cita)
+                .Include(p => p.Resultados)
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public List<Paciente> GetAll()
+        {
+            return _context.Set<Paciente>()
+                .Include(p => p.Cita)
+                .Include(p => p.Resultados)
+                .OrderBy(p => p.Apellidos)
+                .ThenBy(p => p.Nombres)
+                .ToList();
+        }
+
+        public Paciente GetByDni(string dni)
+        {
+            return _context.Set<Paciente>()
+                .Include(p => p.Cita)
+                .Include(p => p.Resultados)
+                .FirstOrDefault(p => p.Dni == dni);
+        }
+
+        public List<Paciente> SearchByNombre(string nombre)
+        {
+            return _context.Set<Paciente>()
+                .Where(p =>
+                    p.Nombres.Contains(nombre) ||
+                    p.Apellidos.Contains(nombre))
+                .OrderBy(p => p.Apellidos)
+                .ThenBy(p => p.Nombres)
+                .ToList();
+        }
+
+        public void Add(Paciente paciente)
+        {
+            _context.Set<Paciente>().Add(paciente);
+            _context.SaveChanges();
+        }
+
+        public void Update(Paciente paciente)
+        {
+            _context.Set<Paciente>().Update(paciente);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var paciente = _context.Set<Paciente>().Find(id);
+            if (paciente != null)
+            {
+                _context.Set<Paciente>().Remove(paciente);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
